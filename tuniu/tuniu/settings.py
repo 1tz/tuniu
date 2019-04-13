811505eef8334bda644a1b27e38fe2c1caa2df08
Rudolflee <ilikemeat@qq.com>
2019-04-13 23:13:17 +0800

    MOD: Quicker

--- tuniu/tuniu/settings.py
+++ tuniu/tuniu/settings.py
@@ -23,14 +23,14 @@ LOG_LEVEL = 'ERROR'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 64
 
 DOWNLOAD_TIMEOUT = 30
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+# DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
